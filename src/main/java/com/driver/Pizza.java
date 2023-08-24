1ffcfc6898ee38ecafc56d87677ad6475acb2d1a
NayanKumarG <59191154+NayanKumarG@users.noreply.github.com>
2023-08-24 11:26:05 +0530

    Pizza bill implemented

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,13 +5,15 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean isCheese = false;
+    private boolean isTopping = false;
+    private boolean isAway = false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
         if(isVeg) this.price = 300;
         else this.price = 400;
-        this.bill = String.valueOf(this.price);
     }
 
     public int getPrice(){
@@ -20,21 +22,58 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        this.bill = String.valueOf(Integer.parseInt(this.bill)+80);
+        this.isCheese = true;
     }
 
     public void addExtraToppings(){
         // your code goes here
-        this.bill = String.valueOf(Integer.parseInt(this.bill)+(isVeg==true?70:120));
+        this.isTopping = true;
     }
 
     public void addTakeaway(){
         // your code goes here
-        this.bill = String.valueOf(Integer.parseInt(this.bill)+20);
+        this.isAway = true;
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        System.out.println("Base Price Of The Pizza:"+this.getPrice());
+        if(this.isVeg)
+        {
+            if(this.isCheese)
+            {
+                this.price+=80;
+                System.out.println("Extra Cheese Added:"+80);
+            }
+            if(this.isTopping)
+            {
+                System.out.println("Extra Toppings Added:"+70);
+                this.price+=70;
+            }
+            if(this.isAway)
+            {
+                System.out.println("Paperbag Added:"+20);
+                this.price+=20;
+            }
+        }
+        else
+        {
+            if(this.isCheese)
+            {
+                this.price+=80;
+                System.out.println("Extra Cheese Added:"+80);
+            }
+            if(this.isTopping)
+            {
+                System.out.println("Extra Toppings Added:"+120);
+                this.price+=120;
+            }
+            if(this.isAway)
+            {
+                System.out.println("Paperbag Added:"+20);
+                this.price+=20;
+            }
+        }
+        return "Total Price:"+this.price;
     }
 }
